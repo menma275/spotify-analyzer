603a406191ac6b9f4845abf74139f6192bd96c1c
menma275 <ohsusie9@gmail.com>
2024-12-01 20:29:26 +0100

    add metadata and analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,12 @@ import { Inter } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
 
+const siteName = "Spotify Analyzer";
+const description = "Analize Your Listeing Experience on Spotify";
+const url = "https://sptf-anlz.vercel.app/";
+
+import { Analytics } from "@vercel/analytics/react";
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -18,7 +24,36 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Spotify Analizer",
-  description: "Analize Your Listeing Experience on Spotify",
+  description: description,
+  openGraph: {
+    title: "Spotify Analyzer",
+    description,
+    url,
+    siteName,
+    locale: "en",
+    type: "website",
+    images: [
+      {
+        url: `${url}opengraph-image.jpg`,
+        width: 1200,
+        height: 630,
+        alt: siteName,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteName,
+    description,
+    site: "@menma275",
+    creator: "@menma275",
+    images: {
+      url: `${url}opengraph-image.jpg`,
+      width: 1200,
+      height: 630,
+      alt: siteName,
+    },
+  },
 };
 
 export default function RootLayout({
@@ -28,6 +63,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Analytics />
       <body
         className={`${inter} ${geistSans.variable} ${geistMono.variable}  antialiased`}
       >
