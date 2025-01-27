b67e2ba0dbbe8a68334d4f716d90e1b5672ec9a5
menma275 <ohsusie9@gmail.com>
2025-01-27 11:45:30 +0100

    change animate duration of tiles

--- app/components/ShowRecentlyPlayed.tsx
+++ app/components/ShowRecentlyPlayed.tsx
@@ -33,7 +33,7 @@ export default function ShowRecentlyPlayed() {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
-          transition={{ delay: index * 0.1, duration: 0.25 }}
+          transition={{ delay: index * 0.05, duration: 0.25 }}
           key={`${index}${item.track.id}`}
         >
           <Track
--- app/components/ShowTopArtists.tsx
+++ app/components/ShowTopArtists.tsx
@@ -58,7 +58,7 @@ export default function ShowTopArtists() {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            transition={{ delay: index * 0.1, duration: 0.25 }}
+            transition={{ delay: index * 0.05, duration: 0.25 }}
             key={`${index}${item.id}${timeRange}`}
           >
             <Track
